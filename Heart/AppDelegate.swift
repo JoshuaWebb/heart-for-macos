7a0910727ac427210449a2db18b7c632f6e73204
Joshua Webb <bbew.auhsoj+git@gmail.com>
2018-01-31 22:32:39 +1100

    Initial Commit
    Blank Swift/Cocoa project

---
+++ Heart/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Heart
+//
+//  Created by josh on 31/01/2018.
+//  Copyright (c) 2018 joshuawebb. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Heart/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Heart
+//
+//  Created by josh on 31/01/2018.
+//  Copyright (c) 2018 joshuawebb. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
